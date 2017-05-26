5a42b46ed216b29c8541b87582f62a6988b7891e
Strong9282 <strong9282@fullsail.edu>
2017-05-25 23:40:22 -0400

    SEttings Menu
    Created the settings menu, added the sound settings and set up all of
    the scripts for the audio, player should now be abale to adjust the
    volume of sfx and music seperately.

---
+++ Defend_Yourself1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject settingsMenuCanvas;
+    public Slider[] volumeSliders;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        settingsMenuCanvas.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void SettingsMenu()
+    {
+        settingsMenuCanvas.SetActive(true);
+    }
+
+    public void SettingsMenuAccept()
+    {
+        settingsMenuCanvas.SetActive(false);
+    }
+
+    public void SettingsMenuCancel()
+    {
+        settingsMenuCanvas.SetActive(false);
+    }
+
+    public void SetMasterVolume(float value)
+    {
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Master);
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Music);
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.SFX);
+    }
+
+
+}
